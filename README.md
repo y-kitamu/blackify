cd92cecd31f9facc7f39a7e5e4a0256037365eea
Kitamura <ymyk6602@gmail.com>
2022-02-06 22:46:18 +0900

    add source code and readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Blackify
+
+Blackify is a emacs-lisp function that format python buffer using 
+(black)[https://github.com/psf/black]. The package is inspired by 
+(yapfify)[https://github.com/JorisE/yapfify] and has similar interface to it.
+Yapfify (yapf) can be easily replaced to blackify (black) only by renaming
+"yapfify" to "blackify" in `init.el`.
+
+## Configuration
+
+- sample configuration ((use-package)[https://github.com/jwiegley/use-package] 
++ (straight)[https://github.com/raxod502/straight.el])
+
+```lisp
+(use-package blackify
+    :straight (blackify :type git :host github :repo "y-kitamu/blackify")
+    :hook (python-mode . blackify-mode))
+```
---
+++ blackify.el
@@ -0,0 +1,104 @@
+;;; blackify.el --- format python buffers using black.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Yusuke Kitamura
+
+;; Author: Yusuke Kitamura <ymyk6602@gmail.com>
+;; Keywords: languages
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Blackify uses black to format Python buffer.
+;; Blackify is based on yapfify (https://github.com/JorisE/yapfify) and
+;; has similar (analogous) functions and variables to it.
+
+;;; Code:
+(require 'dash)
+
+(defcustom blackify-executable "black"
+  "Executable used to start yapf."
+  :type 'string
+  :group 'blackify)
+
+(defcustom blackfiy-lighter " black"
+  :type 'string
+  :group 'blackify)
+
+(defun blackify-call-bin (input-buffer output-buffer start-pos end-pos)
+  "Call process black on INPUT-BUFFER saving the output to OUTPUT-BUFFER.
+Return the exit code.  START-LINE and END-LINE specify region to
+format."
+  (with-current-buffer input-buffer
+    (call-process-region start-pos end-pos blackify-executable nil output-buffer nil "--code")))
+
+
+;;;###autoload
+(defun blackify-region (beginning end)
+  "Try to apply black to the current region.
+If black exits with an error, the output will be shown in a help-window."
+  (interactive "r")
+  (-if-let (tmp-buffer (get-buffer "*blackify*"))   ;; Make sure to delete old window
+      (kill-buffer tmp-buffer))
+  (let* ((original-buffer (current-buffer))
+         (original-point (point))  ; Because we are replacing text, save-excursion does not always work.
+         (buffer-windows (get-buffer-window-list original-buffer nil t))
+         (original-window-pos (mapcar 'window-start buffer-windows))
+         (start-pos  (line-beginning-position (line-number-at-pos beginning)))
+         (end-pos (line-end-position (line-number-at-pos end)))
+         (tmpbuf (get-buffer-create "*blackify*"))
+         (exit-code (blackify-call-bin original-buffer tmpbuf start-pos end-pos)))
+    (deactivate-mark)
+    ;; There are three exit-codes defined for YAPF:
+    ;; 0: Exit with success (change or no change on yapf >=0.11)
+    ;; 1: Exit with error
+    ;; 2: Exit with success and change (Backward compatibility)
+    ;; anything else would be very unexpected.
+    (cond ((or (eq exit-code 0) (eq exit-code 2))
+           (with-current-buffer tmpbuf
+             (copy-to-buffer original-buffer (point-min) (point-max))))
+          ((eq exit-code 1)
+           (error "Black failed, see %s buffer for details" (buffer-name tmpbuf))))
+    ;; Clean up tmpbuf
+    (kill-buffer tmpbuf)
+    ;; restore window to similar state
+    (goto-char original-point)
+    (cl-mapcar 'set-window-start buffer-windows original-window-pos)))
+
+
+;;;###autoload
+(defun blackify-buffer ()
+  "Blackify whole buffer."
+  (interactive)
+  (blackify-region (point-min) (point-max)))
+
+
+;;;###autoload
+(defun blackify-region-or-buffer ()
+  "Blackify the region if it is active. Otherwise, blackify the buffer"
+  (interactive)
+  (if (region-active-p)
+      (blackify-region (region-beginning) (region-end))
+    (blackify-buffer)))
+
+;;;###autoload
+(define-minor-mode blackify-mode
+  "Automatically run black before saving."
+  :lighter blackify-lighter
+  (if black-mode
+      (add-hook 'before-save-hook 'blackify-buffer nil t)
+    (remove-hook 'before-save-hook 'blackify-buffer t)))
+
+(provide 'blackify)
+;;; blackify.el ends here
